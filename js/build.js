de67d0b7869510a1249d25411a0c007b19359b34
Maroni19 <maroni101010@gmail.com>
2018-02-02 18:00:14 +0200

    create js

--- js/build.js
+++ js/build.js
@@ -1,4 +1,9 @@
 console.log('start1');
 console.log('start2');
 console.log('start3');
-//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIm1vZHVsZTEuanMiLCJtb2R1bGUyLmpzIiwibW9kdWxlMy5qcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQTtBQ0FBO0FDQUEiLCJmaWxlIjoiYnVpbGQuanMiLCJzb3VyY2VzQ29udGVudCI6WyJjb25zb2xlLmxvZygnc3RhcnQxJyk7IiwiY29uc29sZS5sb2coJ3N0YXJ0MicpOyIsImNvbnNvbGUubG9nKCdzdGFydDMnKTsiXX0=
+$(document).ready(function(){
+    $(".btn1").click(function(){
+        $(".p1").toggle();
+    });
+});
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIm1vZHVsZTEuanMiLCJtb2R1bGUyLmpzIiwibW9kdWxlMy5qcyIsInRvZ2dsZS5qcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQTtBQ0FBO0FDQUE7QUNBQTtBQUNBO0FBQ0E7QUFDQTtBQUNBIiwiZmlsZSI6ImJ1aWxkLmpzIiwic291cmNlc0NvbnRlbnQiOlsiY29uc29sZS5sb2coJ3N0YXJ0MScpOyIsImNvbnNvbGUubG9nKCdzdGFydDInKTsiLCJjb25zb2xlLmxvZygnc3RhcnQzJyk7IiwiJChkb2N1bWVudCkucmVhZHkoZnVuY3Rpb24oKXtcclxuICAgICQoXCIuYnRuMVwiKS5jbGljayhmdW5jdGlvbigpe1xyXG4gICAgICAgICQoXCIucDFcIikudG9nZ2xlKCk7XHJcbiAgICB9KTtcclxufSk7Il19
